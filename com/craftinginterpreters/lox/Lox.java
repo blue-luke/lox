a147fc4d0d41d084fe6b2319e8fda2832d71127f
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2023-06-17 20:18:09 +0100

    feat: basic error handling

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -7,6 +7,8 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+
+  static boolean hadError = false;
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -18,6 +20,9 @@ public class Lox {
     }
   }
   private static void runFile(String path) throws IOException {
+    // Indicate an error in the exit code.
+    if (hadError) System.exit(65);
+
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
   }
@@ -31,6 +36,7 @@ public class Lox {
       String line = reader.readLine();
       if (line == null) break;
       run(line);
+      hadError = false;
     }
   }
 
@@ -42,6 +48,17 @@ public class Lox {
     for (Token token : tokens) {
       System.out.println(token);
     }
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where,
+                             String message) {
+    System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
   }
 
 } 
